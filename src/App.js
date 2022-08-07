e1984d3abaf0822c0356b26d09997bd5b754141c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-07 18:53:27 +0430

    Create home app stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  colors as Colors,
+} from "@mui/material";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from "./components/navbar";
+import HomePage from "./pages/home";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      background: {
+        default: "#f8fbff",
+      },
+      primary: {
+        main: Colors.blueGrey[500],
+      }
+    }
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact><HomePage /></Route>
+          <Route path="/manual" exact>Manual</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
