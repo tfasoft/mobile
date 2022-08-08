230b88fbfc832ac097ae5931b72fa6785a265df6
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-08 09:36:16 +0430

    Remove links and stuff

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,31 +3,7 @@ import {
     Container,
     Toolbar,
     Typography,
-    Button,
 } from "@mui/material";
-
-const linkItems = [
-    {
-        "title": "TFAsoft",
-        "href": "https://tfasoft.amirhossein.info"
-    },
-    {
-        "title": "Dashboard",
-        "href": "https://dashboard.amirhossein.info"
-    },
-    {
-        "title": "Docs",
-        "href": "https://docs.amirhossein.info"
-    },
-    {
-        "title": "Blog",
-        "href": "https://blog.amirhossein.info"
-    },
-    {
-        "title": "Demo",
-        "href": "https://demo.amirhossein.info"
-    },
-];
 
 const Footer = () => {
     return (
@@ -39,27 +15,10 @@ const Footer = () => {
                         fontWeight="bold"
                         sx={{
                             color: "white",
-                            flexGrow: 1,
                         }}
                     >
                         TFAsoft {new Date().getFullYear()} &copy;
                     </Typography>
-                    {
-                        linkItems.map((item) => {
-                            return (
-                                <Button
-                                    variant="text"
-                                    color="inherit"
-                                    href={item.href}
-                                    sx={{
-                                        color: "white"
-                                    }}
-                                >
-                                    {item.title}
-                                </Button>
-                            );
-                        })
-                    }
                 </Toolbar>
             </Container>
         </Box>
